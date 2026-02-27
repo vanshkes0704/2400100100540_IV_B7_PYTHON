65730f45da432a99bf43323e3d5110056345b70e
vanshkes0704 <krishnakes0704@gmail.com>
2026-02-27 11:01:35 +0530

    HI

---
+++ lecture_five.py
@@ -0,0 +1,15 @@
+n = int(input("enter the number :"))
+i = 0
+sum = 0
+while i<=n :
+    sum = sum + i
+    i+=1
+print("total sum =",sum)
+    
+
+info = {
+    "name" : "vansh",
+    "sub" : ["cs","python"],
+    "library" : {'key' : 'value'}
+}
+print(info.keys())
